caa4f92dd7a75bd485dab02d4dd78bc64baefadc
Upendra Gurujala <gurujal.22@gmail.com>
2017-04-22 14:41:15 +0530

    Added basic elements

--- app/src/main/java/com/post2shop/post2shop/MainActivity.java
+++ app/src/main/java/com/post2shop/post2shop/MainActivity.java
@@ -2,6 +2,10 @@ package com.post2shop.post2shop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.logo);
+        imageView.setImageResource(R.drawable.logo);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.category_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        Spinner spinnerSub = (Spinner) findViewById(R.id.subcategory_spinner);
+        ArrayAdapter<CharSequence> adaptersub = ArrayAdapter.createFromResource(this,
+                R.array.subcategory_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSub.setAdapter(adaptersub);
     }
 }
